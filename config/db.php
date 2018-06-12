4f3acc10d6ee99236ff70d629f744ca04fcdb8df
Abdussalam <abdussalam999948@gmail.com>
2018-06-13 01:50:10 +0200

    Last

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=us-cdbr-iron-east-04.cleardb.net;dbname=heroku_863ef3af3f86ae3',
+    'username' => 'b294ac9c09b8f4',
+    'password' => 'b3fe07573730540',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
--- config/test_db.php
+++ config/test_db.php
@@ -1,6 +1,6 @@
 <?php
 $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mysql:host=localhost;dbname=yii2_basic_tests';
+$db['dsn'] = 'mysql:host=us-cdbr-iron-east-04.cleardb.net;dbname=heroku_863ef3af3f86ae3';
 
 return $db;
